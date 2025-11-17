2e110f705b54bb44eb733a2a4f384433dc50187e
Mateo <mateo.aguilar2008@gmail.com>
2025-11-17 03:02:52 -0500

    update

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-
+let memory = [];
 async function runAI(mensajeUsuario) {
         const response = await fetch("/generate", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ mensaje: mensajeUsuario })
+            body: JSON.stringify({ 
+            mensaje: mensajeUsuario,
+            history: memory
+             })
         });
-let memory = [];
+
         const data = await response.json();
         const respuesta = data.output_text;
         
