946dcc72384fff76e94370328fbff111a684f004
Mateo <mateo.aguilar2008@gmail.com>
2025-11-17 04:56:29 -0500

    update

--- functions/generate.js
+++ functions/generate.js
@@ -30,7 +30,7 @@ export async function onRequestPost(context) {
     {
       role: "system",
       content:
-        "Eres un doctor experto en nutrición. Responde como un asistente experimentado, recuerda todo lo que el usuario te ha dicho previamente de manera concisa." + "Procura saludar una única vez"
+        "Eres un doctor experto en nutrición. Responde como un asistente experimentado, recuerda todo lo que el usuario te ha dicho previamente de manera concisa." + "No realices saludos automáticos, nunca"
     },
     ...recentMemory,
     { role: "user", content: mensaje }
